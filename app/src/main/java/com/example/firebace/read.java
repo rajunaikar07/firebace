4e20e9eef60289f16d43a3705454780b9b00911d
rajunaikar07 <110777070+rajunaikar07@users.noreply.github.com>
2022-09-26 11:56:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebace/read.java
@@ -0,0 +1,106 @@
+package com.example.firebace;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class read extends AppCompatActivity {
+    FirebaseFirestore firebaseFirestore;
+    ProgressDialog progressDialog;
+    RecyclerView recyclerView;
+    MyAdapter myadapter;
+
+    List<MyModel> myModelList=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+
+
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        myadapter=new MyAdapter(getApplicationContext(),myModelList);
+        recyclerView.setAdapter(myadapter);
+
+
+
+
+
+
+    }
+
+    public void loaddata(View view) {
+        myModelList.clear();
+        progressDialog.show();
+        progressDialog.setMessage("Loading Data---");
+
+        firebaseFirestore.collection("Hostel").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                progressDialog.dismiss();
+
+                if (!task.isSuccessful())
+                {
+                    Toast.makeText(read.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    for (QueryDocumentSnapshot snapshot:task.getResult())
+                    {
+                        String name=snapshot.getString("Name");
+                        String mobile=snapshot.getString("Mobile");
+                        String mail =snapshot.getString("Mail");
+                        String gender=snapshot.getString("Gender");
+                        String state=snapshot.getString("State");
+
+
+                        MyModel dataModel=new MyModel(name,mobile,mail,gender,state);
+                        myModelList.add(dataModel);
+
+                        myadapter.notifyDataSetChanged();
+
+
+
+
+//                        dataModelList.add(""+name+"\n"+mail+"\n"+mobile+"\n"+gender+"\n"+state);
+//                        dataAdapter.notifyDataSetChanged();
+
+
+
+                    }
+                }
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+
+            }
+        });
+    }
+}
+
